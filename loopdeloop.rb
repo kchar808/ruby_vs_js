2e9267caa5d898aefe4bd4a5bd76356f8a969e44
Keelana Char <keelanachar@gmail.com>
2019-12-06 23:45:01 -0600

    more deliberate ruby practice

--- loopdeloop.rb
+++ loopdeloop.rb
@@ -56,21 +56,77 @@
 # Use a nested loop to find the largest sum of any two different numbers within an array.
 #     For example, [1, 8, 3, 10] becomes 18.
 
-numbers = [1, 8, 3, 10]
-i = 0
-max_sum = numbers[i] + numbers[1]
-while i < numbers.length
-  j = 0
-  while j < numbers.length
-    if i != j
-      sum = numbers[i] + numbers[j]
-      if sum > max_sum
-        max_sum = sum
-      end
-    end 
-    j += 1
-  end
-  i += 1
-end
+# numbers = [1, 8, 3, 10]
+# i = 0
+# max_sum = numbers[i] + numbers[1]
+# while i < numbers.length
+#   j = 0
+#   while j < numbers.length
+#     if i != j
+#       sum = numbers[i] + numbers[j]
+#       if sum > max_sum
+#         max_sum = sum
+#       end
+#     end 
+#     j += 1
+#   end
+#   i += 1
+# end
 
-p max_sum
+# p max_sum
+
+# Use a nested loop with two arrays of numbers to create a new array of the sums of each combination of numbers.
+# For example, [1, 2] and [6, 7, 8] becomes [7, 8, 9, 8, 9, 10].
+
+# one = [1, 2]
+# two = [6, 7, 8]
+# number_sums = []
+# i = 0
+# sum = one[i] + two[0]
+# while i < one.length
+#   j = 0
+#   while j < two.length
+#     number_sums << one[i] + two[j]
+#     j += 1
+#   end
+#   i += 1
+# end
+
+# p number_sums
+
+# Use a nested loop with one array of strings to create a new array that contains every combination of each string with every other string in the array.
+#     For example, ["a", "b", "c", "d"] becomes ["ab", "ac", "ad", "ba", "bc", "bd", "ca", "cb", "cd", "da", "db", "dc"].
+
+# letters = ["a", "b", "c", "d"]
+# letter_combos = []
+# i = 0
+# while i < letters.length
+#   j = 0
+#   while j < letters.length
+#     if letters[i] != letters[j]
+#       letter_combos << letters[i] + letters[j]
+#     end
+#     j += 1
+#   end
+#   i += 1
+# end
+
+# p letter_combos
+
+# Use a nested loop to convert an array of number pairs into a single flattened array.
+# For example, [[1, 3], [8, 9], [2, 16]] becomes [1, 3, 8, 9, 2, 16].
+
+# numbers = [[1, 3], [8, 9], [2, 16]]
+# flattened = []
+# i = 0
+# while i < numbers.length
+#   inner_array = numbers[i]
+#   j = 0
+#   while j < inner_array.length
+#     flattened << inner_array[j]
+#     j += 1
+#   end
+#   i += 1
+# end
+
+# p flattened
